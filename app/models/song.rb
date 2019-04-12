166ad2d6a37645eb041254c76675d059e861ece8
diautzi <dia_utzi@yahoo.com>
2019-04-12 17:27:39 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,5 @@
 class Song < ActiveRecord::Base
-
+belongs_to :artist
+belongs_to :genre
+# belongs_to :record_label
 end
--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,2 +1,9 @@
-class CreateArtists < ActiveRecord::Migration[4.2]
+class CreateArtists < ActiveRecord::Migration[5.2]
+  # Database did not have this table after migration completed. Going to attempt same change in future migration to fix
+
+   # def change
+   #   create_table :artists do |t|
+   #     t.string :name
+   #   end
+   # end
 end
